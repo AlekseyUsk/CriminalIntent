3925e03f35892f14fd453205ae702ebc1572f276
Aleksey <meysonsib200189@mail.ru>
2022-08-21 12:35:47 +0700

    привязал к CrimeListFragment -> RecyclerView и привязал RecyclerView к fragment_crime_list |создал list_item_crime.xml

--- app/src/main/java/com/hfad/criminalintent/controler/CrimeListFragment.kt
+++ app/src/main/java/com/hfad/criminalintent/controler/CrimeListFragment.kt
@@ -2,14 +2,22 @@ package com.hfad.criminalintent.controler
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.hfad.criminalintent.R
 import com.hfad.criminalintent.controler.CrimeListViewModel
 
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
+
+    private lateinit var crimeRecyclerView: RecyclerView
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
@@ -19,8 +27,22 @@ class CrimeListFragment : Fragment() {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Total crimes:${crimeListViewModel.crimes.size}")
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+        crimeRecyclerView =
+            view.findViewById(R.id.crime_recycler_view) as RecyclerView
+        crimeRecyclerView.layoutManager =
+            LinearLayoutManager(context)
+        return view
+    }
+
     companion object {
-        fun newInstance() : CrimeListFragment{
+        fun newInstance(): CrimeListFragment {
             return CrimeListFragment()
         }
     }
