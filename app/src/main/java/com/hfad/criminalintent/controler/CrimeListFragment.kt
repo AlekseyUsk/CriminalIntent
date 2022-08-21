0d3a7269872928c18512c572290bc09a0a7a2d6e
Aleksey <meysonsib200189@mail.ru>
2022-08-21 13:11:40 +0700

    добавил в класс CrimeListFragment ViewHolder и Adapter

--- app/src/main/java/com/hfad/criminalintent/controler/CrimeListFragment.kt
+++ app/src/main/java/com/hfad/criminalintent/controler/CrimeListFragment.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.hfad.criminalintent.R
-import com.hfad.criminalintent.controler.CrimeListViewModel
+import com.hfad.criminalintent.model.Crime
 
 
 private const val TAG = "CrimeListFragment"
@@ -39,6 +41,32 @@ class CrimeListFragment : Fragment() {
         crimeRecyclerView.layoutManager =
             LinearLayoutManager(context)
         return view
+    }
+
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+    }
+
+    private inner class CrimeAdapter(var crimes: List<Crime>) :
+        RecyclerView.Adapter<CrimeHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+            return CrimeHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime = crimes[position]
+            holder.apply {
+                titleTextView.text = crime.title
+                dateTextView.text = crime.date.toString()
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return crimes.size
+        }
+
     }
 
     companion object {
