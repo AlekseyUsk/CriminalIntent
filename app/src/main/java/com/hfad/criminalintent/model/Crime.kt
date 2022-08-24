a9b2350b700798326847f2356cbd52c18e76f708
Aleksey <meysonsib200189@mail.ru>
2022-08-24 14:45:27 +0700

    Определение сущностей Room в классе Crime

--- app/src/main/java/com/hfad/criminalintent/model/Crime.kt
+++ app/src/main/java/com/hfad/criminalintent/model/Crime.kt
@@ -1,9 +1,14 @@
 package com.hfad.criminalintent.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.text.SimpleDateFormat
 import java.util.*
 
+@Entity
 data class Crime(
+    @PrimaryKey // Первичный ключ в базе данных который содержит данные, уникальные для каждой записи или строки id: UUID = UUID.randomUUID()
+
 
     val id: UUID = UUID.randomUUID(),
     var title: String = "",
